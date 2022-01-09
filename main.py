20346e9677a986a7f83479dd0c51984de71eceb4
Ben Herbst <83538916+BenHerbst@users.noreply.github.com>
2022-01-09 16:25:09 +0100

    Aktualisiere pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+basic.showString("Tamaliope")
 _4Digit = grove.create_display(DigitalPin.C16, DigitalPin.C17)
 flush = tamaliope_creation.tamaliope("Floush",
     images.create_image("""
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+basic.showString("Tamaliope")
 let _4Digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 let flush = new tamaliope_creation.tamaliope("Floush", images.createImage(`
 . # # # .
