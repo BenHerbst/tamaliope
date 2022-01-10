3b4cc164a63a6d5aefd26538c8fcdb83f998dab3
Ben Herbst <83538916+BenHerbst@users.noreply.github.com>
2022-01-10 17:05:51 +0100

    Aktualisiere pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-_4Digit = grove.create_display(DigitalPin.C16, DigitalPin.C17)
+_4digit = grove.create_display(DigitalPin.C16, DigitalPin.C17)
+_4digit.set(7)
+serial.write_line("" + ("Welcome to Tamaliope - 0.01\nWrite help or info."))
+basic.show_string("Tamaliope")
 flush = tamaliope_creation.tamaliope("Floush",
     images.create_image("""
 . # # # .
